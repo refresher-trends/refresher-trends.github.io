c2d9e8d84b91bdc64ff323a8c3c3c45de71ea691
Matheus Ferreira <matheusfs97@gmail.com>
2023-03-16 16:04:34 -0300

    database: updated json

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,6 @@
 # insomnia.json alterations
 sed -i 's/{% response 'body', 'req_365ff04d8dc14e80ab49e51d24f922c7', 'b64::JFswXS5rZXk=::46b', 'always', 60 %}/bVloK2p0RGp2NmtydXB6K3dudkJLZz09/g' docs/insomnia.json
+sed -i 's/{% response 'body', 'req_8cbcef67dafb44e9ae6d6e4e5cce7d68', 'b64::JC50b2tlbg==::46b', 'never', 60 %}/NJLGNILKJZBUlK4kYpEF59H5is0zLv8YRpR2R1s0eGap9feqFXwUPD9WGRco/g' docs/insomnia.json
 
 insomnia-documenter -c "docs/insomnia.json" -f "../core/public/images/icon.png" -o "docs/" -l "../core/public/images/logo1.png" -d "."
 
